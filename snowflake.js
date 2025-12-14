3273f6028f2f297e088e2c259fd232612f360dde
Ivana <118979116+IvanaErin@users.noreply.github.com>
2025-12-14 12:18:09 +0800

    Update and rename supabase.js to snowflake.js

---
+++ snowflake.js
@@ -0,0 +1,13 @@
+import snowflake from "snowflake-sdk";
+
+export function getConnection() {
+  return snowflake.createConnection({
+    account: process.env.SNOWFLAKE_ACCOUNT,
+    username: process.env.SNOWFLAKE_USERNAME,
+    password: process.env.SNOWFLAKE_PASSWORD,
+    database: process.env.SNOWFLAKE_DATABASE,
+    schema: process.env.SNOWFLAKE_SCHEMA,
+    warehouse: process.env.SNOWFLAKE_WAREHOUSE,
+    role: process.env.SNOWFLAKE_ROLE,
+  });
+}
