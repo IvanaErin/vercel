25ed15321134d9900504fe8c2c360eafba190162
Ivana <118979116+IvanaErin@users.noreply.github.com>
2025-12-14 15:38:57 +0800

    Update me.js

--- api/me.js
+++ api/me.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 export default function handler(req, res) {
   const auth = req.headers.authorization;
-  if (!auth) return res.status(401).json({ success:false });
+  if (!auth) return res.status(401).json({ success: false });
 
   try {
     const token = auth.split(" ")[1];
     const user = jwt.verify(token, process.env.JWT_SECRET);
-    res.json({ success:true, user });
+    res.json({ success: true, user });
   } catch {
-    res.status(401).json({ success:false });
+    res.status(401).json({ success: false });
   }
 }
